c06098bfe0cf5959ba9ec4df07bd113a64d59214
parikls <porovozls@gmail.com>
2019-01-15 11:18:28 +0200

    fix create task alias

--- mem_usage_ui/__init__.py
+++ mem_usage_ui/__init__.py
@@ -9,4 +9,4 @@ __version__ = '0.8'
 
 if sys.version_info.minor < 7:
     # alias for previous python versions
-    asyncio.create_task = lambda coro: asyncio.get_event_loop().create_task(coro)
+    asyncio.create_task = lambda coro, loop=None: (loop if loop else asyncio.get_event_loop()).create_task(coro)
